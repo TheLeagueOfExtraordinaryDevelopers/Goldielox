8646e7a3783872cc8aa96c69b9b67b45f3166471
Rashiki <rashiki@yahoo.com>
2015-12-05 17:08:38 -0500

    Share db, implement a slider for tracking position

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+var sqlite3      = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('goldielox.db');
 
 var routes = require('./routes/index');
 var users  = require('./routes/users');
@@ -13,6 +16,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// set database
+app.set('db', db);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var app = req.app;
+  var db  = app.get('db');
+
+  //db.each("SELECT id, name, path FROM tracks", function(err, row) {
+
+  //}
+
+
+  db.all("SELECT id, name, path FROM tracks", function(err, rows) {
+    var tracks = rows;
+
+    res.render('index', { title: 'Express', tracks: tracks });
+  })
+
+
+  //res.render('index', { title: 'Express', db: db });
 });
 
 module.exports = router;
