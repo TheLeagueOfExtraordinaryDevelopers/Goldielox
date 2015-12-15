45778b3c4bb0954fda45a4cf8db96d1f02a335f4
Rashiki <rashiki@yahoo.com>
2015-12-14 19:11:10 -0500

    Ensure only saving the track title and path to the playlist

--- public/javascripts/goldielox.js
+++ public/javascripts/goldielox.js
@@ -208,12 +208,15 @@
     function link($scope, $el, attrs) {
 
       function onkeydown(e) {
+        // Ensure saving just the title and path
+        var tracks = _.map(playlist.tracks, function(track) { return _.pick(track, 'title', 'path') })
+
         if (e.ctrlKey == true && e.keyCode == 83) { // ctrl-s
           $.ajax({
             type: "PUT",
             url: "/playlist.json",
             contentType: 'application/json',
-            data: JSON.stringify(playlist.tracks)
+            data: JSON.stringify(tracks)
           });
         }
       }
